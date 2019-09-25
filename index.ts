c5beed1ae980a6951dc742f0bfc767cd6142716b
to-gth <to.code01.g@gmail.com>
2019-09-25 18:06:07 +0900

    index.ts

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ import _Natural from './src/Natural'
 import _Int from './src/Int'
 import _NaturalInt from './src/NaturalInt'
 
-namespace TsNumber {
-  export type Real = _Real
-  export type Natural = _Natural
-  export type Int = _Int
-  export type NaturalInt= _NaturalInt
-}
-
-export default TsNumber
+export type Real = _Real
+export const Real = _Real
+export type Natural = _Natural
+export const Natural = _Natural
+export type Int = _Int
+export const Int = _Int
+export type NaturalInt= _NaturalInt
+export const NaturalInt= _NaturalInt
