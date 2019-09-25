045ecf305a81a27cd5929459ed3075191686ba5c
to-gth <to.code01.g@gmail.com>
2019-09-25 13:50:05 +0900

    index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-
 import Real from './src/Real'
+import Natural from './src/Natural'
+import Int from './src/Int'
+import NaturalInt from './src/NaturalInt'
 
-export default { Real }
+export default { Real, Natural, Int, NaturalInt }
